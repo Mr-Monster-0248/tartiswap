3896400bb860cf04f71a88792cee512d37dcbd02
AmiralBl3ndic <camille.briand@gmail.com>
2022-03-10 18:59:39 +0100

    chore: remove old NextJS dex app
    Replaces it with the SvelteKit app

--- apps/dex/wallets/connectors/network.ts
+++
@@ -1,9 +0,0 @@
-import { NetworkConnector } from '@web3-react/network-connector';
-export const network = new NetworkConnector({
-  urls: {
-    1: 'https://mainnet.infura.io/v3/802b576107de4648a78e369a3b467d93',
-    3: 'https://ropsten.infura.io/v3/802b576107de4648a78e369a3b467d93',
-    4: 'https://rinkeby.infura.io/v3/802b576107de4648a78e369a3b467d93',
-  },
-  defaultChainId: 4,
-});
