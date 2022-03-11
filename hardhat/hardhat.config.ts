397239c31f66f39a4c0f9929bfb8006702c2726e
Thibault LEPEZ <thibault.lepez@efrei.net>
2022-03-11 12:10:34 +0100

    feat(rinkeby): add rinkeby network

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -30,6 +30,14 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gas: 4000000,
+      gasPrice: "auto",
+      gasMultiplier: 2,
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
